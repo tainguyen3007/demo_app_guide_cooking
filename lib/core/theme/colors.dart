6405353a438adb4ea215fe329c59038b3f8d4859
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-08-20 01:07:26 +0700

    add app color

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  /// Blue shades - Secondary
+  static const Map<int, Color> blue = {
+    50:  Color(0xFFE9F9FF),
+    100: Color(0xFFCEF0FF),
+    200: Color(0xFFA7E6FF),
+    300: Color(0xFF6BDBFF),
+    400: Color(0xFF26C2FF),
+    500: Color(0xFF009AFF),
+    600: Color(0xFF0070FF),
+    700: Color(0xFF0055FF),
+    800: Color(0xFF0048E6),
+    900: Color(0xFF0043B3),
+    950: Color(0xFF002D73),
+  };
+
+  /// Yellow shades - Primary
+  static const Map<int, Color> yellow = {
+    50:  Color(0xFFFDFEE8),
+    100: Color(0xFFFCFFC2),
+    200: Color(0xFFFCFF87),
+    300: Color(0xFFFFFC43),
+    400: Color(0xFFFFEE0A),
+    500: Color(0xFFEFD503),
+    600: Color(0xFFCEA700),
+    700: Color(0xFFA47804),
+    800: Color(0xFF885D0B),
+    900: Color(0xFF734C10),
+    950: Color(0xFF432805),
+  };
+
+  /// Gray shades - Neuture
+  static const Map<int, Color> gray = {
+    50:  Color(0xFFF8F8F8),
+    100: Color(0xFFF1EFEF),
+    200: Color(0xFFE5E3E3),
+    300: Color(0xFFD3CECE),
+    400: Color(0xFFB8B1B1),
+    500: Color(0xFF9E9595),
+    600: Color(0xFF898080),
+    700: Color(0xFF6E6767),
+    800: Color(0xFF5D5757),
+    900: Color(0xFF504C4C),
+    950: Color(0xFF292626),
+  };
+}
