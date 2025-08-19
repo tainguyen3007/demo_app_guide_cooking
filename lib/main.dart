8665d0498f82dc001df17b5bedeb8bd4d3e01e7f
22DH113189 - Việt Tài <nguyenviettai770@gmail.com>
2025-08-20 01:08:17 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:demo_app_food_booking/screen/home_screen.dart';
-import 'package:demo_app_food_booking/screen/splash_screen.dart';
+import 'package:demo_app_food_booking/features/follow_guide_cooking/screens/detail_guide_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:demo_app_food_booking/features/home/screens.dart';
+import 'package:demo_app_food_booking/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +14,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Demo Splash Screen',
-      initialRoute: '/',
+      initialRoute: '/', // Trang khởi tạo
       routes: {
-        '/': (context) => const SplashScreen(),
-        '/home': (context) => const HomeScreen(),
+        '/': (context) => const SplashScreen(), 
+        '/main': (context) => const MainScaffold(),
       },
     );
   }
 }
 
+class MainScaffold extends StatefulWidget {
+  const MainScaffold({super.key});
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    Center(child: Text("Search Page")),
+    Center(child: Text("Saved Page")),
+    Center(child: Text("Account Page")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search_outlined),
+            selectedIcon: Icon(Icons.search),
+            label: "Search",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bookmark_outline),
+            selectedIcon: Icon(Icons.bookmark),
+            label: "Saved",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+      ),
+    );
+  }
+}
